ac14d5c611bf1a2c691cdb61cc79c612eccfcb1d
abab2320 <1589841436@qq.com>
2025-02-21 18:44:25 +0800

    作业1“

---
+++ test1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace System
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("你好，我是王雨菲");
+        }
+    }
+
+}
